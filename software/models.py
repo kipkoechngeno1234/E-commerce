a0af99cdf0f9e7801346ca2f4b204002c08baa9a
Finch Kyle <aron.ngenok@gmail.com>
2025-09-24 23:13:43 +0300

    Created  destination model

--- software/models.py
+++ software/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
